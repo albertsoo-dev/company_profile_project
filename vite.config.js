68e8594458995f717f6291625f9ed0a287496918
Albert Soo <25265104+albertsoo-dev@users.noreply.github.com>
2025-12-20 06:58:43 +0800

    Rename project to company_profile_01

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/company_profile_project/",
+  base: "/company_profile_01/",
   build: {
     outDir: "docs",
     emptyOutDir: true,
